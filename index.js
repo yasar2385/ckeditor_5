be5cce4b880713dcc5e513c515c7401309c1cea9
yasar2385 <61082958+yasar2385@users.noreply.github.com>
2023-08-25 00:42:05 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
+var editor;
 setTimeout(function () {
-  ClassicEditor.create(document.querySelector('#editor')).catch((error) => {
-    console.error(error);
+  ClassicEditor.create(document.querySelector('#editor'))
+    .then((newEditor) => {
+      editor = newEditor;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  // Assuming there is a <button id="submit">Submit</button> in your application.
+  document.querySelector('#submit').addEventListener('click', () => {
+    const editorData = editor.getData();
+
+    // ...
   });
 }, 7500);
